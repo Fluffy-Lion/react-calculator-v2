20fec73f0648877be95612a285fa86672a916f6c
Fluffy-Lion <leoncooper31@gmail.com>
2021-01-15 10:50:40 +0000

    short version using map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
+import './App.css';
+import { evaluate } from 'mathjs'
+
+class App extends Component {
+
+  state = {
+    total: "",
+    buttons: ["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="]
+  }
+
+  clickHandler = (value) => {
+    if(value === "="){
+    this.setState({total: evaluate(this.state.total) })
+  }else if (value === "c"){
+    this.setState({total: ""})
+  }else {
+    this.setState({total: this.state.total + value})
+  }
+  }
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>appjs</h1>
+      <h2>{this.state.total}</h2>
+      {this.state.buttons.map((button, index) => {
+        return ( 
+            <Button 
+            value={button} 
+            key={index}
+            clickHandler={this.clickHandler}
+            /> 
+        )
+      })}
     </div>
   );
+}
+}
+
+const Button = (props) => {
+  return(
+      <button onClick={() => props.clickHandler(props.value)}>
+        {props.value}
+      </button>
+  )
 }
 
 export default App;
